ba6b5d9789d86ac82e4e7a1949a61b741d7eab77
huilaribeiro85 <48697004+huilaribeiro85@users.noreply.github.com>
2019-03-31 13:21:29 -0300

    Update run.py

--- cut_processor/run.py
+++ cut_processor/run.py
@@ -4,27 +4,36 @@ import time
 import _thread as thread
 from cut_processor.cut_processor import web_server, video_processor, log_reader
 
-logs_path = os.path.join(os.getcwd(), "logs")
-videos_to_process_path = os.path.join(os.getcwd(), r"videos_to_process")
-processed_videos_path = os.path.join(os.getcwd(), r"processed_videos")
+class Processor():
+    def __init__(self, log_path=os.path.join(os.getcwd(), "logs"),
+                 videos_to_process_path=os.path.join(os.getcwd(), r"videos_to_process"),
+                 processed_videos_path=os.path.join(os.getcwd(), r"processed_videos")):
+        self.logs_path = log_path
+        self.videos_to_process_path = videos_to_process_path
+        self.processed_videos_path = processed_videos_path
 
 
-def run_web_server():
-    web_server_path = os.path.abspath(web_server.__file__)
-    subprocess.run(["python", web_server_path])
+    def run_web_server(self):
+        web_server_path = os.path.abspath(web_server.__file__)
+        subprocess.run(["python", web_server_path])
 
 
-def run_video_processor():
-    video_processor.VideoProcessor(logs_path, videos_to_process_path, processed_videos_path).run()
+    def run_video_processor(self):
+        video_processor.VideoProcessor(self.logs_path, self.videos_to_process_path, self.processed_videos_path).run()
 
 
-thread.start_new_thread(run_web_server)
-thread.start_new_thread(run_video_processor)
+    def start_threads(self):
+        thread.start_new_thread(self.run_web_server)
+        thread.start_new_thread(self.run_video_processor)
 
-log = ""
-while True:
-    time.sleep(10)
-    start_log_reader = log_reader.GetLog(logs_path).run()
-    if start_log_reader and log != start_log_reader:
-        log = start_log_reader
-        print(start_log_reader)
+        log = ""
+        while True:
+            time.sleep(10)
+            start_log_reader = log_reader.GetLog(self.logs_path).run()
+            if start_log_reader and log != start_log_reader:
+                log = start_log_reader
+                print(start_log_reader)
+
+if __name__ == "__main__":
+    p = Processor()
+    p.start_threads()
