701b9558a5c91133be582bd2503bac72ce36f53d
huilaribeiro85 <48697004+huilaribeiro85@users.noreply.github.com>
2019-03-31 10:30:55 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 
 setup(
-    name='cut_video_processor',
+    name='video_processor',
     version='1.0.0',
     author='Huila Ribeiro',
     packages=find_packages(exclude=['tests']),
