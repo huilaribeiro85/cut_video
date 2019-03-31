f7409fac29fcf35b348e418bcfbaeec6d538a7df
huilaribeiro85 <huila.ribeiro@gmail.com>
2019-03-31 09:26:33 -0300

    edit setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-from moviepy.video.io import ffmpeg_tools, VideoFileClip
+
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 
-def get_video_size_by_time(video_file):
-    clip = VideoFileClip.VideoFileClip(video_file)
-    clip_time = clip.duration
-    clip.close()
-    return clip_time
-
-
-def cut_video(input_video, output_video, start_time=0, end_time=30):
-    ffmpeg_tools.ffmpeg_extract_subclip(input_video, start_time, end_time, targetname=output_video)
+setup(
+    name='cut_video',
+    version='1.0.0',
+    author='Huila Ribeiro',
+    install_requires=['requests', 'flask', 'moviepy']
+)
